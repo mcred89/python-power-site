00502f890e6906aaa5986aa8715fc3a61b5c0697
mac <seanmcilroy89@gmail.com>
2018-08-05 20:27:54 -0500

    Upgrading to use flask native forms

--- index.py
+++ index.py
@@ -1,25 +1,47 @@
-from flask import Flask, render_template, request, url_for
+import os
+
+from flask import Flask, render_template, request, url_for, flash, redirect
+from flask_wtf.csrf import CSRFProtect
+from forms import RegistrationForm, LoginForm, MaxesForm
+from utils import get_secret_key
+
+
 
 app = Flask(__name__)
 
+# CSRF Needs to be reendabled.
+# The site works fine locally but I'm gett he below error in dev:
+# KeyError: 'A secret key is required to use CSRF.'
+#
+# I've tried everything in flask-wtf docs to get this working and\
+# everything in the 3 (tiny) forum posts that I can find
+app.config['WTF_CSRF_ENABLED'] = False
+
+secret_name = os.getenv('ENV_SECRET_NAME')
+secret_key = os.getenv('ENV_SECRET_KEY')
+region = os.getenv('ENV_REGION')
+
+app.secret_key = get_secret_key(secret_name, secret_key, region)
 
 @app.route("/", methods=['GET', 'POST'])
-def pagecreate():
-  return render_template('index.html')
+def home():
+  form = MaxesForm()
+  return render_template('maxinput.html', form=form)
 
 
 @app.route("/workout", methods=['GET', 'POST'])
 def workoutcreate():
+  form = MaxesForm(request.form)
   try:
-    msq = float(request.form['msq'])
-    mbe = float(request.form['mbe'])
-    mdl = float(request.form['mdl'])
+    msq = float(form.msq.data)
+    mbe = float(form.mbe.data)
+    mdl = float(form.mdl.data)
   except:
     return render_template('error.html')
-  mainliftchoice = str(request.form['mainliftchoice'])
+  mainliftchoice = form.mainliftchoice.data
   from workout import workoutscript
   outputs = workoutscript(msq, mbe, mdl, mainliftchoice)
-  return render_template('output.html', outputs=outputs)
+  return render_template('output.html', outputs=outputs, form=form)
 
 @app.route("/calculators")
 def calculators():
@@ -33,13 +55,24 @@ def otherprograms():
 def about():
   return render_template('about.html')
 
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
-  return render_template('login.html')
+  form = LoginForm()
+  if form.validate_on_submit():
+    if form.email.data == 'test@test.com' and form.password.data == 'password':
+      flash('You have been logged in!', 'success')
+      return redirect(url_for('home'))
+    else:
+      flash('Login Failed', 'danger')
+  return render_template('login.html', form=form)
 
-@app.route("/register")
+@app.route("/register", methods=['GET', 'POST'])
 def register():
-  return render_template('register.html')
+  form = RegistrationForm()
+  if form.validate_on_submit():
+    flash(f'Account created for {form.username.data}', 'success')
+    return redirect(url_for('home'))
+  return render_template('register.html', form=form)
 
 
 if __name__ == "__main__":
