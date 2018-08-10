31217031907c983638b9e580889c2d8f957f1c0e
mac <seanmcilroy89@gmail.com>
2018-08-09 21:38:28 -0500

    Removed username. Just using email.

--- forms.py
+++ forms.py
@@ -4,8 +4,6 @@ from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 
 class RegistrationForm(FlaskForm):
-    username = StringField('Username',
-                           validators=[DataRequired(), Length(min=2, max=20)])
     email = StringField('Email',
                         validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
